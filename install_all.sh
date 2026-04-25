cd1fb3d19c714c21bace34cfa0e3282d870fddb1
Git-Monke <javelasq2006@gmail.com>
2026-04-25 12:57:03 -0700

    Added some plugin shit

--- install_all.sh
+++ install_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-pi install npm:pi-extension-manager &
-pi install npm:pi-subagents &
-pi install npm:@tmustier/pi-usage-extension &
-pi install npm:@studiosunnyfield/pimagotchi &
-pi install npm:pi-connect &
-pi install npm:pi-animations &
-pi install npm:pi-tokensaver &
-pi install npm:@plannotator/pi-extension &
-cargo install tokensave &
+pi install npm:pi-extension-manager
+pi install npm:pi-subagents
+pi install npm:@tmustier/pi-usage-extension
+pi install npm:@studiosunnyfield/pimagotchi
+pi install npm:pi-connect
+pi install npm:pi-animations
+pi install npm:pi-cursor-provider
+pi install npm:pi-jingle
+pi install npm:pi-vim
 
 wait
 echo "All plugins installed!"
